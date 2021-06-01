cc57da3c334db3bcfa2501ec52ca374f5069d739
Justin Yu <628snapdragon@gmail.com>
2021-06-01 02:15:37 -0500

    Better Linux window selection

--- nestris_ocr/capturing/linux/linux_mgr.py
+++ nestris_ocr/capturing/linux/linux_mgr.py
@@ -18,24 +18,48 @@ class WindowMgr:
         Return a list of tuples (handler, window name) for each real window.
         """
         windows = []
+        display = Xlib.display.Display()
+        root = display.screen().root
 
-        def getWindowHierarchy(window, windows):
-            children = window.query_tree().children
-            for w in children:
-                try:
-                    w.get_image(0, 0, 1, 1, X.ZPixmap, 0xFFFFFFFF)
-                    windows.append(
-                        (
-                            w.id,
-                            w.get_wm_class()[1] if w.get_wm_class() is not None else "",
-                        )
-                    )
-                except Xlib.error.BadMatch:
-                    pass
-                finally:
-                    windows = getWindowHierarchy(w, windows)
+        try:
+            _NET_CLIENT_LIST = display.get_atom('_NET_CLIENT_LIST')
+            _NET_WM_NAME = display.get_atom('_NET_WM_NAME')
+            client_list = root.get_full_property(
+                _NET_CLIENT_LIST,
+                property_type=X.AnyPropertyType,
+            ).value
+
+            for window_id in client_list:
+                window = display.create_resource_object('window', window_id)
+                window_name = window.get_full_property(
+                    _NET_WM_NAME,
+                    property_type=X.AnyPropertyType,
+                ).value.decode('utf-8')
+                windows.append((window_id, window_name))
             return windows
 
-        root = Xlib.display.Display().screen().root
-        windows = getWindowHierarchy(root, windows)
-        return windows
+        except Xlib.error.BadAtom:
+            windows = []
+            def getWindowHierarchy(window, windows):
+                children = window.query_tree().children
+                for w in children:
+                    try:
+                        w.get_image(0, 0, 1, 1, X.ZPixmap, 0xFFFFFFFF)
+                        wname = ""
+                        if w.get_wm_name() is not None:
+                        	wname += w.get_wm_name()
+                        if w.get_wm_class() is not None:
+                        	wname += w.get_wm_class()[1]
+                        windows.append(
+                            (
+                                w.id,
+                                wname,
+                            )
+                        )
+                    except Xlib.error.BadMatch:
+                        pass
+                    finally:
+                        windows = getWindowHierarchy(w, windows)
+                return windows
+            windows = getWindowHierarchy(root, windows)
+            return windows
